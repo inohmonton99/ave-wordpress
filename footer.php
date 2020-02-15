682fd1113f0aff740a8c964e8d1808b8a4cc1c4f
inohmonton99 <inohfrancismonton@gmail.com>
2020-02-15 10:28:29 +0800

    initial commit

---
+++ footer.php
@@ -0,0 +1,87 @@
+<footer class="main-footer pt-80">
+
+	<section>
+
+		<div class="container">
+			<div class="row">
+	
+				<div class="lqd-column col-md-3 col-sm-6">
+	
+					<h3 class="widget-title">Navigation</h3>
+					<ul class="lqd-custom-menu reset-ul">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">Product</a></li>
+						<li><a href="#">Customers</a></li>
+						<li><a href="#">Pricing</a></li>
+					</ul>
+	
+				</div><!-- /.col-md-3 col-sm-6 -->
+	
+				<div class="lqd-column col-md-3 col-sm-6">
+	
+					<h3 class="widget-title">Abous Us</h3>
+					<ul class="lqd-custom-menu reset-ul">
+						<li><a href="#">Company</a></li>
+						<li><a href="#">Jobs</a></li>
+						<li><a href="#">Press</a></li>
+						<li><a href="#">Blog</a></li>
+					</ul>
+	
+				</div><!-- /.col-md-3 col-sm-6 -->
+	
+				<div class="lqd-column col-md-3 col-sm-6">
+	
+					<h3 class="widget-title">Workflows</h3>
+					<ul class="lqd-custom-menu reset-ul">
+						<li><a href="#">Management</a></li>
+						<li><a href="#">Reporting</a></li>
+						<li><a href="#">Tracking</a></li>
+						<li><a href="#">All Users</a></li>
+					</ul>
+						
+				</div><!-- /.col-md-3 col-sm-6 -->
+					
+				<div class="lqd-column col-md-3 col-sm-6">
+
+					<h3 class="widget-title">Resources</h3>
+					<ul class="lqd-custom-menu reset-ul">
+						<li><a href="#">Ave Guide</a></li>
+						<li><a href="#">Support</a></li>
+						<li><a href="#">Intergration</a></li>
+						<li><a href="#">Communnity</a></li>
+					</ul>
+	
+				</div><!-- /.col-md-3 col-sm-6 -->
+	
+			</div><!-- /.row -->
+		</div><!-- /.container -->
+
+	</section>
+
+	<section class="bt-fade-white-015 pt-90 pb-90">
+		<div class="container">
+			<div class="row">
+	
+				<div class="lqd-column col-md-6 col-md-offset-3 text-center">
+	
+					<figure class="mb-40">
+						<img src="./assets/img/logo/logo-1.svg" alt="Logo">
+					</figure>
+
+					<p>© 2019 Ave. All Rights Reserved.</p>
+	
+				</div><!-- /.col-md-6 -->
+	
+			</div><!-- /.row -->
+		</div><!-- /.container -->
+	</section>
+		
+</footer><!-- /.main-footer -->
+</div><!-- /#wrap -->
+
+<script src="./assets/vendors/jquery.min.js"></script>
+<script src="./assets/js/theme-vendors.js"></script>
+<script src="./assets/js/theme.min.js"></script>
+
+</body>
+</html>
